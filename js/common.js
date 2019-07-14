998ce97bc70eff8f9527ec4eecc033abdac09cb8
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-07-14 22:25:55 +0300

    full

---
+++ js/common.js
@@ -0,0 +1,66 @@
+// slider
+
+let retroSlider = function () {
+  let slider = document.querySelector('.slider')
+  let sliderInner = slider.querySelector('.slider-inner');
+  let nextBtn = slider.querySelector('.slider-control--next');
+  let prevBtn = slider.querySelector('.slider-control--prev');
+
+  let sliderItems = sliderInner.children;
+  let count = 0;
+  for (let i = 1; i < sliderItems.length; i++) {
+    sliderItems[i].style.display = 'none'
+  }
+  slider.addEventListener('click', function (e) {
+    if (e.target == nextBtn) {
+      getNext();
+    }
+    if (e.target == prevBtn) {
+      getPrev();
+    }
+  })
+  let getNext = () => {
+    if (count == sliderItems.length - 1) {
+      count = 0;
+    }
+    else {
+      count++;
+    }
+    showItems();
+    sliderItems[count].style.display = 'block';
+  }
+  let getPrev = () => {
+    if (count == 0) {
+      count = sliderItems.length - 1;
+    }
+    else {
+      count--;
+    }
+    showItems();
+    sliderItems[count].style.display = 'block';
+  }
+  let showItems = () => {
+    for (let i = 0; i < sliderItems.length; i++) {
+      sliderItems[i].style.display = 'none'
+    }
+  }
+}
+
+retroSlider()
+
+
+// header
+
+let fixedHeader = function () {
+  let header = document.querySelector('.header');
+  window.addEventListener('scroll', e => {
+    if (window.scrollY > 10) {
+      header.querySelector('.header-menu-logo').style.top = '-10px'
+    }
+    else {
+      header.querySelector('.header-menu-logo').style.top = '20px'
+    }
+  })
+}
+
+fixedHeader()
